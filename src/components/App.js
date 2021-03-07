82dceced4bfba96803f89326912f67e30b8b805e
Daniel Vaughn <daniel.vaughn@whiteops.com>
2021-03-07 17:55:16 -0500

    adds initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { fonts } from '../theme'
+
+const StyledDiv = styled.div`
+  color: pink;
+  font-family: ${fonts.body};
+`
+
+const App = () => {
+  return <StyledDiv>hello world</StyledDiv>
+}
+
+export default App
